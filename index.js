819dbd66bc818091768cadb89011606769fdaefd
admin <admin@admins-MacBook-Pro-2.local>
2019-08-15 13:47:28 -0700

    create html table and list stream inside of table.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let listMin = 0;
+let listMax = 5;
 let pageIndex = 1;
 
-let streamData = (name) => {
+let json = (name) => {
     return fetch(`https://api.twitch.tv/kraken/search/streams?query=${name}&limit=${55}`, {
         method: 'GET', 
         mode: 'cors', 
@@ -17,9 +19,9 @@ let streamData = (name) => {
     .catch(error => console.error(error))
 };
 
-let onSearch = () => {
+let onSearch = (func) => {
     let gameName = document.getElementById("searchbar").value;
-    streamData(gameName).then((parsedJson) => {
+    json(gameName).then((parsedJson) => {
         let total = parsedJson._total
         let list = parsedJson.streams.map((element)=> {
             return {
@@ -32,6 +34,57 @@ let onSearch = () => {
                 viewers: element.viewers,
             };
         });
-        console.log(list)
+        func(listMin, listMax, list);
     });
+};
+
+let listFive = (min, max, func) => {
+    let table = document.getElementById("table");
+    while (table.firstChild) {
+    table.removeChild(table.firstChild);
+    }
+    for (let i = min; i < max; i++) {
+        let row = document.createElement("div");
+        row.setAttribute("class", "row");
+        let firstColumn = document.createElement("div");
+        firstColumn.setAttribute("class", "col-1");
+        let img = document.createElement("img");
+        img.setAttribute("src", func[i]["image"]);
+        img.setAttribute("width", "100%");
+        img.setAttribute("height", "120px")
+        firstColumn.appendChild(img);
+        row.appendChild(firstColumn);
+        let secondColumn = document.createElement("div");
+        secondColumn.setAttribute("class", "col-2");
+        secondColumn.innerHTML = `
+          <div class="display-name">
+            <a href="${func[i]["url"]}">${func[i]["displayName"]}</a>
+          </div>
+          <p class="details">
+            <span>${func[i]["game"]} - ${func[i]["viewers"]}</span>
+            <span>${func[i]["description"]}</span>
+          </p>
+          `
+        row.appendChild(secondColumn);
+        table.appendChild(row);
+    }
+};
+
+let onNext = () => {
+    if(listMax !== 50) {
+        listMin = listMax;
+        listMax += 5;
+        onSearch(listFive);
+        pageIndex++
+    };
+    
+};
+
+let onPrevious = () => {
+    if (listMin !== 0) {
+        listMax -= 5;
+        listMin -= 5;
+        onSearch(listFive);
+        pageIndex--
+    };
 };
